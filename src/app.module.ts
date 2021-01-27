003ea7037db9eec368c2241a5358df1b86437657
sebasttian.baquero <sebastian.baquero@accenture.com>
2021-01-27 02:02:18 -0500

    ajuste conexion a mongo local con el nombre de la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ShipmentSchema } from './shipment/schemas/shipment.schema';
 import { ShipmentModule } from './shipment/shipment.module';
 
 @Module({
@@ -12,8 +11,7 @@ import { ShipmentModule } from './shipment/shipment.module';
     //   useUnifiedTopology: true,
     //   useCreateIndex: true,
     // }),
-    MongooseModule.forFeature([{ name: 'Shipment', schema: ShipmentSchema }]),
-    MongooseModule.forRoot('mongodb://localhost:27017/test', {useNewUrlParser: true}),
+    MongooseModule.forRoot('mongodb://localhost:27017/ppAcigol', {useNewUrlParser: true}),
     ShipmentModule,
   ],
   controllers: [AppController],
